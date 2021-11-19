a0678a412baf2b93a737ef270330ee647a77d320
lflondonor <lflondonor@unal.edu.co>
2021-11-18 19:45:47 -0500

    first commit

---
+++ app/src/main/java/com/example/tiendadonemiliop67/view/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tiendadonemiliop67.view.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tiendadonemiliop67.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
