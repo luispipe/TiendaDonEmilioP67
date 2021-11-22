e39144d68d92864dbda1ac20ad66dd09d0b509f5
lflondonor <lflondonor@unal.edu.co>
2021-11-21 22:30:59 -0500

    guide 12 commit

--- app/src/main/java/com/example/tiendadonemiliop67/view/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/tiendadonemiliop67/view/ui/activities/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.tiendadonemiliop67.view.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.tiendadonemiliop67.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.example.tiendadonemiliop67.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val REQUEST_PERMISSIONS_REQUEST_CODE = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        configNav()
+    }
+
+    fun configNav(){
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragContent) as NavHostFragment
+        val navController = navHostFragment.navController
+        findViewById<BottomNavigationView>(R.id.bnvMenu).setupWithNavController(navController)
     }
 }
